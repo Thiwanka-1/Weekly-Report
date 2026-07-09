45b8110e834b3b8aebe35a953124909519a63c53
Gamithu Arunod <128372987+Thiwanka-1@users.noreply.github.com>
2026-07-09 20:24:58 +0530

    created user controller

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,26 @@
+import User from '../models/User.js';
+
+// Get all users (Useful for Manager dashboard filters)
+export const getUsers = async (req, res) => {
+    try {
+        // We only want to send back the names, IDs, and roles, not passwords!
+        const users = await User.find({}).select('-password');
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get current logged-in user profile
+export const getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select('-password');
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
